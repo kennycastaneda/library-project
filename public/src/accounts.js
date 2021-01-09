8b6bce2e9e024625243650873175cb116b91475f
Kenny Castaneda <kenny.castaneda@gmail.com>
2021-01-09 12:45:07 -0600

    Kenny Castaneda

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,55 @@
-function findAccountById(accounts, id) {}
+function accountId (account){
+  const accountId = account.id;
+  return accountId;
+}
+function newBookAuthor(book = {}, authors = []){ //creates a new object with author inserted into book object
+  const id = book.authorId; //get the author id from the book
+  let foundAuthor = authors.find((author) => author["id"] === id); //if author id is found in authors, return author object
+  book["author"] = foundAuthor; //add author key with foundAuthor value
+  return book;//return the book that now has new author key
 
-function sortAccountsByLastName(accounts) {}
+}
 
-function numberOfBorrows(account, books) {}
+function findAccountById(accounts, id) {
+  let result = accounts.find((account) => account.id === id);//gives the account value with key id found
+  return result;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function sortAccountsByLastName(accounts) {
+  accounts.sort((accountA,accountB) => (accountA.name.last > accountB.name.last ? 1 : -1)); //sorts by comparing last names in each object by pairs
+  return accounts;
+}
+
+function numberOfBorrows(account, books) {
+  const accId = (accountId(account)); //make a const value of account ID
+  //console.log(accountId)
+  let count = 0; //initiate counter for result to return
+  for (book of books){ //iterate through each book
+    let borrowed = book.borrows; //pull out the array of borrows
+    //console.log(borrowed);
+    for (borrowId of borrowed){ //iterate through each id found in borrows of each book
+      if (borrowId.id === accId) count += 1; //add to counter if there is a match
+    }
+  }
+  return count; //return the total matches
+}
+
+function getBooksPossessedByAccount(account, books, authors) {
+  const accId = (accountId(account)); //get accId using helper function
+  let bookAuthor = []; //set up array for book objects with author object inside
+  for (book of books){ //iterate through the books array objects
+    let borrowed = book.borrows; //pull out the array of borrows
+    for (borrowId of borrowed){ //iterate through the borrowed objects
+      if(borrowId.id === accId){ //see if the account is in the borrows
+        if(!borrowId.returned) { //if it is and the book is not returned 
+          let pushBookAuthor = newBookAuthor(book,authors); //helper function to bring in author into array
+          bookAuthor.push(pushBookAuthor); //insert array with book object that has author object inserted
+        }
+      }
+    }
+  }
+  return bookAuthor;
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,35 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  let foundAuthor = authors.find((author) => author["id"] === id); //if author id is found in authors, return author object
+  return foundAuthor;//return the author object
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  let foundBook = books.find((book) => book.id === id);
+  return foundBook; //return the book object
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let borrowedBooks = [];//set up array for borrowed books
+  let returnedBooks = [];//set up array for returned books
+    for (let book of books){ //iterate through the books array objects
+      let borrowed = book.borrows; //pull out the array of borrows
+      (borrowed.some((borrow) => borrow.returned === false)) ? borrowedBooks.push(book) : returnedBooks.push(book);
+    }
+  return [borrowedBooks, returnedBooks];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  const borrows = book.borrows;
+  console.log (borrows);
+  let borrowersForBook = [];
+  for(let borrow of borrows){
+    let accountBorrow = accounts.find((account) => account.id === borrow.id);
+    accountBorrow["returned"]=borrow.returned;
+    borrowersForBook.push(accountBorrow);
+    if (borrowersForBook.length === 10 ) break;
+  }
+  return borrowersForBook;
+}
 
 module.exports = {
   findAuthorById,
